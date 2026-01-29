229fb8535ac856a282fd9a2f1afa79817712c854
Sarthak Parajuli <sarthak.p@acidintegrations.com>
2026-01-29 23:22:34 +0545

    service fix

--- lib/services/recipe_service.dart
+++ lib/services/recipe_service.dart
@@ -10,7 +10,7 @@ class RecipeService {
   Future<RecipeModel> getRecipes() async {
     try {
       Response apiResponse = await dio.get('https://dummyjson.com/recipes');
-      return RecipeModel.fromJson(jsonDecode(jsonEncode(apiResponse.data)));
+      return RecipeModel.fromJson(apiResponse.data);
     } on DioException catch (e) {
       // return RecipeModel();
       log("Error While calling for recipe API: $e");
